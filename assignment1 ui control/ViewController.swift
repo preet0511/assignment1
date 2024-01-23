6202a25a3fcd65d281c128196d353f35e550509e
preet0511 <preetxdhanjal@gmail.com>
2024-01-23 11:23:34 -0800

    Initial Commit

---
+++ assignment1 ui control/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment1 ui control
+//
+//  Created by user240051 on 1/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
